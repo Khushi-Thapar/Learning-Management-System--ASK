be765c77f57a89373223252ebf681e17ae6e1289
Khushi Thapar <76108322+Khushi-Thapar@users.noreply.github.com>
2022-12-21 12:56:45 +0530

    database created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    course_name = models.CharField(max_length=200)
+    course_id = models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.course_name+" "+self.course_id
+
+class Student(models.Model):
+    name=models.CharField(max_length=200)
+    course_id= models.ForeignKey(Course, null=True, default=0, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.name+" "+self.course_id
+
+class Register(models.Model):
+    mobile= models.CharField(max_length=200)
+    user_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=200)
+    password= models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.email+" "+self.password+" "+self.mobile+" "+self.user_name
+
+  
